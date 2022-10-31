01c5386f3d96bb697bacdfeec77457727bd55c97
kunj-shah <kunjshah130203@gmail.com>
2022-11-01 01:02:03 +0530

    v1

--- README.md
+++
@@ -1,1 +0,0 @@
-# Shoekeens-sgp
---
+++ wp-content/uploads/custom-css-js/260.js
@@ -0,0 +1,2 @@
+<!-- start Simple Custom CSS and JS -->
+<script>(function(w, d) { w.CollectId = "635ff591869a903d0b7e3305"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script><!-- end Simple Custom CSS and JS -->
